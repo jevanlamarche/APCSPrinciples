2bda3c2e3af64390edef56e1c9f5d7d835c469d1
jevanlamarche <57370887+jevanlamarche@users.noreply.github.com>
2019-11-04 11:55:56 -0800

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,18 @@
+import random
+
+randonumber = random.randint(1000,2000)
+print (randonumber)
+
+
+
+for i in range(1):
+    userinput = input ("Pick a number between 1000 and 2000")
+    if int(userinput) != (randonumber):
+        print ("You dumb dumb, Try again...")
+    elif int(userinput) == (randonumber):
+        print ("Lucky Guess")
+
+booleandata = True
+
+while booleandata = bool(input)
+
